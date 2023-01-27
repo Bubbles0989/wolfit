247236e4d780a1cb80d63e8eed333134f02008e4
Jasper <jasmith17@mail.wou.edu>
2023-01-26 17:04:03 -0800

    added test_helpers.py and added starter tests for less_than_day method

---
+++ tests/unit/test_helpers.py
@@ -0,0 +1,20 @@
+import textwrap
+from datetime import timedelta
+
+from app import db
+from app.helpers import less_than_day, pretty_date
+
+def test_second_diff_less_60():
+    pass
+
+def test_second_diff_less_120():
+    pass
+
+def test_second_diff_less_3600():
+    pass
+
+def test_second_diff_less_7200():
+    pass
+
+def test_second_diff_less_86400():
+    pass
