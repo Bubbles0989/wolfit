5ac0d3bd5955a8cf1954fbafd513187bf0e0f2ac
Jasper <jasmith17@mail.wou.edu>
2023-01-26 20:25:40 -0800

    finished some pretty_date tests, added more bare ones. helpers.py at 89% cov

--- tests/unit/test_helpers.py
+++ tests/unit/test_helpers.py
@@ -1,5 +1,6 @@
 from datetime import timedelta
 from datetime import datetime
+from datetime import date
 
 from app import db
 from app.helpers import less_than_day, pretty_date
@@ -33,8 +34,37 @@ def test_second_diff_less_86400():
 
 # pretty date
 
-def test_time_is_int_returns_now():
+def test_time_is_int():
+    now = datetime.utcnow()
+    test_time = 1549836078
+    test_diff = now - datetime.fromtimestamp(test_time)
+    day_diff = test_diff.days
+
+    return_string = pretty_date(test_time)
+    assert return_string == str(day_diff // 365) + " years ago"
+
+def test_time_is_false():
+    test_time = False
+    return_string = pretty_date(test_time)
+    assert return_string == "just now"
+
+def test_time_is_less_than_0_more_than_neg_1():
     pass
 
-def test_time_is_now():
+def test_time_is_yesterday():
+    today = datetime.utcnow()
+    yesterday = today - timedelta(days = 1)
+    return_string = pretty_date(yesterday)
+    assert return_string == "Yesterday"
+
+def test_time_is_days_ago():
+    pass
+
+def test_time_is_weeks_ago():
+    pass
+
+def test_time_is_months_ago():
+    pass
+
+def test_time_is_years_ago():
     pass
