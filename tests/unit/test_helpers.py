3e175701478d6ad727d928294dc7d4d5bb5e98fc
Jasper <jasmith17@mail.wou.edu>
2023-01-26 17:29:53 -0800

    all less_than_day tests pass

--- tests/unit/test_helpers.py
+++ tests/unit/test_helpers.py
@@ -5,16 +5,24 @@ from app import db
 from app.helpers import less_than_day, pretty_date
 
 def test_second_diff_less_60():
-    pass
+    new_int = 45
+    new_string = less_than_day(new_int)
+    assert new_string == str(new_int) + " seconds ago"
 
 def test_second_diff_less_120():
-    pass
+    new_int = 110
+    new_string = less_than_day(new_int)
+    assert new_string == "a minute ago"
 
 def test_second_diff_less_3600():
-    pass
+    new_int = 2350
+    new_string = less_than_day(new_int)
+    assert new_string == str(new_int // 60) + " minutes ago"
 
 def test_second_diff_less_7200():
-    pass
+    new_int = 3687
+    new_string = less_than_day(new_int)
+    assert new_string == "an hour ago"
 
 def test_second_diff_less_86400():
     new_int = 77777
